d1e76a931e4905837e8c39d55984b347218d2e5b
aylinulaba <aylinulaba@icloud.com>
2025-09-02 23:58:29 +0300

    chore: purge repository contents

--- assets/js/main.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){
-  var focusables=document.querySelectorAll("a,button,input,select,textarea");
-  focusables.forEach(function(el){el.addEventListener("keydown",function(e){if(e.key==="Enter"&&el.tagName==="A"){e.preventDefault()}})})
-})
